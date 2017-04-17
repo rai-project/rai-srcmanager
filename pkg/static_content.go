417f59d8f83ad9849d6fc9d8942e85fbddee1b61
Abdul Dakkak <abduld@wolfram.com>
2017-04-17 12:09:59 -0500

    updates

--- pkg/static_content.go
+++ pkg/static_content.go
@@ -190,14 +190,14 @@ var _escData = map[string]*_escFile{
 
 	"/repositories": {
 		local:   "../repositories",
-		size:    1271,
-		modtime: 1490972471,
+		size:    1267,
+		modtime: 1492448974,
 		compressed: `
-H4sIAAAAAAAA/3ySUY7DMAhE//cubaW90IrY1KHBxsI4m+zpV5X6O/lDemPMDBSJdS73ZPXhJLfu9uIU
-D/K0ys5fCM9YIfsdCC1uGzuiSYVbQFozRNaeUi5o8IH7zkzSnoZ4pqCFBowiW7rwxJVEESz2nhzTbMFt
-x9yKclKbMJiVSfGiNCdKKzSmVgo2VjlcEtx0tczQd9slC91GlWvFDx/dPPAQXaXBHt3tKQrt9bmMuSDq
-BPu+643Pwu1K8anvH4nA+3ISuD+nYJUq8Hidi4zwE/HBLqTyhyMc7PsFDXOY4AhnqpCeI7iqpQ0pwinh
-r2eIwvOaE4e2S2cPKvDx8foe/wEAAP//AfyxMvcEAAA=
+H4sIAAAAAAAA/3ySUW7DMAxD/3eXrsAuNCi26qiVLUOWvWSnH4p9M38BniKTFIvEPrfPZPXuJLfu9uQU
+d/K0y+IPhGfskP0MhDa3FzuiSYVbQFozRNYeUi5o8IH3zkzSHoZ4pqCNBowiW7rwxJVEESz2Vo5ptuC2
+MLeinNQmDGZnUnwozYnSDo2plYKNVQ6XBC9dLTP03ZZkoduocj3xzUc3DyyiqzS4o7s9RKG9PrcxN0Sd
+4N7394vPwu1q4r8Wt2U6K9TgJPB2TsEqVWBxnYuM8BPxwS6k8ovjG+zrgoY5VD7CmSqk5wiuaumFJsIp
+4adniMJqzYlDW9LZgwr8+Xh+jb8AAAD//4JhF2PzBAAA
 `,
 	},
 
