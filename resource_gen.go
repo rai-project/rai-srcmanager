a576f67a546140f78678cc20c992b65dc0c96712
Abdul Dakkak <abduld@wolfram.com>
2018-09-08 11:53:00 -0500

    update

--- resource_gen.go
+++ resource_gen.go
@@ -1,3 +1,4 @@
+//go:generate go get -v github.com/mjibson/esc
 //go:generate esc -o cmd/static_content.go -pkg cmd -private LICENSE.TXT
 //go:generate esc -o pkg/static_content.go -pkg srcmanager -private repositories
 
